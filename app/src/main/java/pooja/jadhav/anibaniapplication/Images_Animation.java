cb607d03787ec1bf8277797a6a05d0dc30690a0d
Kenisha <jadhavpoojasopan@gmail.com>
2020-08-07 11:12:06 +0530

    Bottom Navigation

--- app/src/main/java/pooja/jadhav/anibaniapplication/Images_Animation.java
+++ app/src/main/java/pooja/jadhav/anibaniapplication/Images_Animation.java
@@ -13,12 +13,18 @@ public class Images_Animation extends Activity {
         setContentView(R.layout.activity_images_animation);
 
         ImageView img = findViewById(R.id.img_aimation);
-
-
         AnimationDrawable animationDrawable = (AnimationDrawable) img.getBackground();
         animationDrawable.setEnterFadeDuration(3000);
         animationDrawable.setExitFadeDuration(1000);
         animationDrawable.start();
+        // Load the ImageView that will host the animation and
+        // set its background to our AnimationDrawable XML resource.
+        ImageView img1 = findViewById(R.id.imageView);
+        img1.setBackgroundResource(R.drawable.spin);
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) img1.getBackground();
+        // Start the animation (looped playback by default).
+        frameAnimation.start();
 
     }
 }
--- app/src/main/java/pooja/jadhav/anibaniapplication/MainActivity.java
+++ app/src/main/java/pooja/jadhav/anibaniapplication/MainActivity.java
@@ -5,16 +5,16 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
     Button show_images;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+       // BottomNavigationView navView = findViewById(R.id.nav_bottom);
         show_images = findViewById(R.id.show_img_btns);
-
-
         show_images.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,13 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void Show_Color_animation(View view) {
         Intent colors = new Intent(MainActivity.this,Color_Aniamtion.class);
         startActivity(colors);
     }
-
-
 
     public void show_other_anamtion(View view) {
         Intent other = new Intent(MainActivity.this,Other_Aniamtion.class);
